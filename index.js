d339908835e6dcdb17b7b2e2a55ff05c0875cbc4
princeraj863 <princeraj863@gmail.com>
2021-09-09 21:26:38 +0530

    first commit:create structure and setup express server

---
+++ index.js
@@ -0,0 +1,15 @@
+ const express=require('express');
+ const app= express();
+ const port = 8000;
+
+
+ app.listen(port,function(err){
+ 
+    if(err){
+        console.log(`error in running the server:${port}`);
+        // here backticks i.e `` inside it works as string until to use variable inside string use ${variable}
+    }
+
+    console.log(`server is running on port:${port}`);
+
+ });
