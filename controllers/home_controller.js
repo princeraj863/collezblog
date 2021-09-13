c2827fae8c0bc85510dc40acdccebcc8e73e8f0e
princeraj863 <princeraj863@gmail.com>
2021-09-13 20:04:00 +0530

    added display posts with name by populating the user of each post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -7,11 +7,22 @@ const Post = require("../models/post");
 //to export a function which is publicly available to routes
 module.exports.home = function(req,res){
     
-    Post.find({},function(err,posts){
+    // Post.find({},function(err,posts){  TO COMMENT ALL SELECTED LINE USE CTRL +K+C
+    //     return res.render('home',{
+    //         title : "Home",
+    //         posts :posts
+    //      });
+    // });
+
+    //populate the user of each post i.e we can now get get posts.user.name 
+
+    Post.find({}).populate('user').exec(function(err,posts){
         return res.render('home',{
-            title : "Home",
-            posts :posts
-         });
+                    title : "Home",
+                    posts :posts
+             });
+
     });
+
     
 }
