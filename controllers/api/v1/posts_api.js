37f79adb0ab08873ca2b4819fd9f88b92f361586
princeraj863 <princeraj863@gmail.com>
2021-10-03 12:08:04 +0530

    added post(delete,get) api without authentication

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,7 +1,45 @@
-module.exports.index = function(req,res){
-    console.log("hello");
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function(req,res){
+    
+
+    let posts= await Post.find({})
+    .sort('-createdAt') //sort the post according to the nearest post created by me by time  i.e nearest to me
+     .populate('user')
+     .populate({
+         path:'comments', //nested populating , to get comment and user of that comment
+         populate:{
+             path:'user'// further populate user , this way we can do further population
+         }
+     });
+
+
     return res.json(200,{
         message :"List of posts",
-        posts:[]
-    })
+        posts:posts
+    });
 }
+
+module.exports.destroy = async function(req,res){
+
+    try{
+   let post= await Post.findById(req.params.id);
+        //.id means converting the object id into string , before it was req.user._id
+        //if(post.user==req.user.id){ // checking if it's user's own post
+          post.remove();
+           
+       await Comment.deleteMany({post:req.params.id}); // deleting comments related to post
+       //console.log(req.params.id);
+       
+         return res.json(200,{
+           message :"Post and associated comments deleted successfully "
+         });
+
+  }catch(err){
+      console.log(err);
+    return res.json(500,{
+       message : "Interval Server Error"
+    });
+     }
+  }
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -5,5 +5,5 @@ const router = express.Router();// it helps in separating routes and controller
 const postApi = require("../../../controllers/api/v1/posts_api");
 
 router.get('/',postApi.index);
-
+router.delete('/:id',postApi.destroy);
 module.exports= router;
